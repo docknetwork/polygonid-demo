fda4c2e1f96137406e4042e71cc286e5b77a8c25
Mike Parkhill <mike@dock.io>
2023-07-14 16:29:50 -0400

    Cleaned up the UI a bit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 export default function Home() {
-  const [issuerName, setIssuerName] = useState('Dock Polygon ID Issuer');
+  const [issuerName, setIssuerName] = useState();
   const [issuerProfile, setIssuerProfile] = useState();
   const [credentialData, setCredentialData] = useState(
     {
@@ -17,7 +17,6 @@ export default function Home() {
 
   async function handleGenerateProfileSubmit(e) {
     e.preventDefault();
-    console.log(issuerName);
     const { data } = await axios.post('api/create-did/', { issuerName });
     console.log(data);
     setIssuerProfile(data);
@@ -25,7 +24,6 @@ export default function Home() {
 
   async function handleCreateCredentialRequest(e) {
     e.preventDefault();
-    console.log(credentialData);
     const credential = {
       schema: credentialData.schema,
       issuer: issuerProfile.did,
@@ -42,67 +40,10 @@ export default function Home() {
     setClaimQR(data.qrUrl);
   }
 
-  if (claimQR) {
-    console.log(claimQR);
-    return (
-      <>
-        <div className="flex flex-col-reverse lg:flex-row lg:h-screen">
-          <div
-            className="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8"
-            style={{ width: '100%' }}>
-            <div className="w-full px-8 md:px-32 lg:px-24">
-              <p className="font-bold text-2xl">
-                Share this URL with the claim holder to claim the credential.
-                <br />
-                <br />
-                <a href={claimQR} target='_blank' rel="noopener noreferrer">{claimQR}</a>
-              </p>
-              <br />
-              <br />
-          </div>
-        </div>
-        </div>
-      </>
-
-    );
-  }
-
   if (!issuerProfile) {
     return (
       <>
         <div className="flex flex-col-reverse lg:flex-row lg:h-screen">
-          <div
-            className="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8"
-            style={{ width: '100%' }}>
-            <div className="w-full px-8 md:px-32 lg:px-24">
-              <form
-                onSubmit={handleGenerateProfileSubmit}
-                className="p-5"
-                method="GET"
-                action="/dashboard"
-                style={{ maxWidth: '500px', margin: '0 auto' }}>
-                <h1 className="text-gray-800 font-bold text-2xl mb-6">Create a Polygon Issuer</h1>
-                <div className="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
-                  <input
-                    id="name"
-                    className=" pl-2 w-full outline-none border-none"
-                    name="name"
-                    placeholder="Issuer Name"
-                    value={issuerName}
-                    onChange={(event) =>
-                      setIssuerName(event.target.value)}
-                  />
-                </div>
-
-                <button
-                  type="submit"
-                  className="block w-full bg-blue-600 mt-5 py-2 rounded-full hover:bg-blue-700 hover:-translate-y-1 transition-all duration-250 text-white font-semibold mb-2">
-                  Create
-                </button>
-
-              </form>
-            </div>
-          </div>
           <div
             className="lg:flex w-full lg:w-1/2 justify-around items-center text-center bg-zinc-900"
             style={{
@@ -118,7 +59,7 @@ export default function Home() {
                 width: '100%',
               }}>
               <h1 className="text-white font-bold text-4xl">
-                Demo Verifiable Credential technology for trusted digital interactions
+                Dock  Polygon ID Demo
               </h1>
 
               <p className="text-white mt-1">
@@ -129,13 +70,74 @@ export default function Home() {
               <br />
             </div>
           </div>
+          <div
+            className="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8"
+            style={{ width: '100%' }}>
+            <div className="w-full px-8 md:px-32 lg:px-24">
+              <form
+                onSubmit={handleGenerateProfileSubmit}
+                className="p-5"
+                style={{ maxWidth: '500px', margin: '0 auto' }}>
+                <h1 className="text-gray-800 font-bold text-2xl mb-6">Create a Polygon Issuer</h1>
+                <div className="flex items-right">
+                Issuer Name:
+                </div>
+                <div className="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
+                  <input
+                    id="name"
+                    className=" pl-2 w-full outline-none border-none"
+                    name="name"
+                    placeholder="Issuer Name"
+                    value={issuerName}
+                    onChange={(event) =>
+                      setIssuerName(event.target.value)}
+                  />
+                </div>
+
+                <button
+                  disabled={!issuerName}
+                  type="submit"
+                  className="block w-full bg-blue-600 mt-5 py-2 rounded-full hover:bg-blue-700 hover:-translate-y-1 transition-all duration-250 text-white font-semibold mb-2">
+                  Create
+                </button>
+
+              </form>
+            </div>
+          </div>
         </div>
       </>
     );
   }
+
+  if (!claimQR) {
     return (
       <>
         <div className="flex flex-col-reverse lg:flex-row lg:h-screen">
+          <div
+            className="lg:flex w-full lg:w-1/2 justify-around items-center text-center bg-zinc-900"
+            style={{
+              flexShrink: 0,
+              background: 'url(/bg.jpg)',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+            }}>
+            <div
+              className="w-full mx-auto px-0 py-10 flex-col items-center space-y-6"
+              style={{
+                maxWidth: '400px',
+                width: '100%',
+              }}>
+              <h1 className="text-white font-bold text-4xl">
+                KYC Age Credential Example
+              </h1>
+
+              <p className="text-white mt-1">
+                We&apos;ll issue a KYC Age Credential for which we need a birthdate.
+              </p>
+              <br />
+              <br />
+            </div>
+          </div>
           <div
             className="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8"
             style={{ width: '100%' }}>
@@ -144,7 +146,7 @@ export default function Home() {
                 onSubmit={handleCreateCredentialRequest}
                 className="p-5"
                 style={{ maxWidth: '500px', margin: '0 auto' }}>
-                <h1 className="text-gray-800 font-bold text-2xl mb-6">Setup KYC Age Credential Request</h1>
+                <h1 className="text-gray-800 font-bold text-2xl mb-6">Enter a date of birth</h1>
                 <div className="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
                   <DatePicker
                     id="dob"
@@ -166,6 +168,7 @@ export default function Home() {
 
                 <button
                   type="submit"
+                  disabled={!credentialData.subject.dob}
                   className="block w-full bg-blue-600 mt-5 py-2 rounded-full hover:bg-blue-700 hover:-translate-y-1 transition-all duration-250 text-white font-semibold mb-2">
                   Create Credential Request
                 </button>
@@ -175,5 +178,56 @@ export default function Home() {
           </div>
         </div>
       </>
+    );
+  }
+
+    return (
+      <>
+        <div className="flex flex-col-reverse lg:flex-row lg:h-screen">
+          <div
+            className="lg:flex w-full lg:w-1/2 justify-around items-center text-center bg-zinc-900"
+            style={{
+              flexShrink: 0,
+              background: 'url(/bg.jpg)',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+            }}>
+            <div
+              className="w-full mx-auto px-0 py-10 flex-col items-center space-y-6"
+              style={{
+                maxWidth: '400px',
+                width: '100%',
+              }}>
+              <h1 className="text-white font-bold text-4xl">
+                Share the claim request link
+              </h1>
+
+              <p className="text-white mt-1">
+                Share this link with the recipient. They will be presented with a QR code they can scan to begin the import
+                flow in their Polygon ID wallet.
+              </p>
+              <br />
+              <br />
+            </div>
+          </div>
+          <div
+            className="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8"
+            style={{ width: '100%' }}>
+            <div className="w-full px-8 md:px-32 lg:px-24">
+              <p className="font-bold text-4xl">
+                Claim URL
+              </p>
+
+              <p>
+                <br />
+                <a href={claimQR} target='_blank' rel="noopener noreferrer">{claimQR}</a>
+              </p>
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+      </>
+
     );
 }
